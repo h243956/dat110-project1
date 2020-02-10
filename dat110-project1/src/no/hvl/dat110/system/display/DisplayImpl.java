d810daf402083a516251a80b773fd8199220e6ee
Mikal Noss <577007@stud.hvl.no>
2020-02-10 15:23:26 +0100

    DisplayDevice og DisplayImpl

--- dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
+++ dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
@@ -18,10 +18,14 @@ public class DisplayImpl implements RPCImpl {
 		// TODO: 
 		// implement unmarshalling, call, and marshall for write RPC method
 		// look at how this is done int he SensorImpl for the read method
+		String message = RPCUtils.unmarshallString(request);
+		write(message);
+		rpcid = request[0];
+		reply = RPCUtils.marshallString(rpcid, message);
 		
-		if (true) {
+		/*if (true) {
 			throw new UnsupportedOperationException(TODO.method());
-		}
+		}*/
 		
 		return reply;
 	}
